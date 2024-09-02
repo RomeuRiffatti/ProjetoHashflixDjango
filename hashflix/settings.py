e0c756dc5f846af658fe99732aa75584434ba5bd
Romeu Riffatti <romeufftt@gmail.com>
2024-09-02 13:45:56 -0300

    ajustando media

--- hashflix/settings.py
+++ hashflix/settings.py
@@ -148,6 +148,7 @@ STATICFILES_DIRS = [
 MEDIA_URL = '/media/'
 
 MEDIA_ROOT = BASE_DIR / "media"
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
