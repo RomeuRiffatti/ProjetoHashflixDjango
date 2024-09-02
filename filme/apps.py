917fa241e46f1d22915bedb65113988b74705a08
Romeu Riffatti <romeufftt@gmail.com>
2024-09-02 12:48:41 -0300

    adicionando o signals para adicionar o super user depois da migração

--- filme/apps.py
+++ filme/apps.py
@@ -1,16 +1,8 @@
 from django.apps import AppConfig
-
-
+import .signals
 class FilmeConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'filme'
 
     def ready(self):
-        from .models import Usuario
-        import os
-        email = os.getenv('EMAIL_ADMIN')
-        senha = os.getenv('SENHA_ADMIN')
-        usuarios = Usuario.objects.filter(email=email)
-        if not usuarios:
-            Usuario.objects.create_superuser(username='admin', email=email, password=senha,
-                                             is_active=True, is_staff=True)
+        import filme.signals  # Importe o arquivo de sinais
