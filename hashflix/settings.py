d0abac670107849d5856837d85cdc940cac8e3dc
Romeu Riffatti <romeufftt@gmail.com>
2024-09-02 12:16:39 -0300

    ajustando Procfile

--- hashflix/settings.py
+++ hashflix/settings.py
@@ -86,18 +86,21 @@ WSGI_APPLICATION = 'hashflix.wsgi.application'
 
 import dj_database_url
 
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
+
+
 DATABASE_URL = os.getenv('DATABASE_URL')
 if DATABASE_URL:
     DATABASES = {
         'default': dj_database_url.config(default=DATABASE_URL,conn_max_age=1800),
     }
-else: 
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / 'db.sqlite3',
-        }
-    }
+
+
     
 
 # Password validation
