2e371ec3734ce14e78ac0a2a819245c7790927b1
Romeu Riffatti <romeufftt@gmail.com>
2024-09-02 13:23:43 -0300

    ajustando imagens

--- filme/models.py
+++ filme/models.py
@@ -13,7 +13,7 @@ LISTA_CATEGORIAS = (
 # criar o filme
 class Filme(models.Model):
     titulo = models.CharField(max_length=100)
-    thumb = models.ImageField(upload_to='thumb_filmes')
+    thumb = models.ImageField(upload_to='thumb_filmes/')
     descricao = models.TextField(max_length=1000)
     categoria = models.CharField(max_length=15, choices=LISTA_CATEGORIAS)
     visualizacoes = models.IntegerField(default=0)
