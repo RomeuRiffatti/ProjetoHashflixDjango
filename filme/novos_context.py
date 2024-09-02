f02924c45a1febf7f57a4189c376b74a7ae1a9e7
Romeu Riffatti <romeufftt@gmail.com>
2024-09-02 12:30:33 -0300

    ajustando Procfile

--- filme/novos_context.py
+++ filme/novos_context.py
@@ -13,3 +13,5 @@ def lista_filmes_recentes(request):
 def lista_filmes_emalta(request):
     lista_filmes = Filme.objects.all().order_by('-visualizacoes')[0:8]
     return {"lista_filmes_emalta": lista_filmes}
+
+#
